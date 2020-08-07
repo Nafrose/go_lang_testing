c73166a7e1897ea9c51c2eb949194fc5a964c3f8
Nafrose <59255287+Nafrose@users.noreply.github.com>
2020-08-07 18:54:26 +0600

    Add files via upload

---
+++ reverse.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(reverse("123456"))
+
+}
+
+func reverse(s string) string {
+	len := len(s)
+
+	var r = make([]byte, len)
+
+	for i := 0; i < len; i++ {
+		r[i] = s[len-1-i]
+	}
+
+	reverse := string(r)
+
+	return reverse
+}
