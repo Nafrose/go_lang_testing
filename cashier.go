fbc53fa61ae679461953ad3c1657ce7ea8e0dc95
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-10 15:40:05 +0600

    Updates

--- cashier.go
+++ cashier.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func cashier2(given int) {
+	notesPossbilities := [9]int{1000, 500, 100, 50, 20, 10, 5, 2, 1}
+	
+
+}
+
 func cashier(t float64) {
 	tInt := int(t * 100)
 
@@ -12,9 +18,11 @@ func cashier(t float64) {
 
 	var m int = q + d + n + p
 
-	fmt.Printf("The cashier may give %d quarte(s), %d dime(s), %d nickel(s) and %d penny(ies) which totals to %d coins", q, d, n, p, m)
+	fmt.Printf("The cashier(%d)\n", t)
+	fmt.Printf("may give %d quarte(s)\n %d dime(s)\n %d nickel(s) \n %d penny(ies) which totals to %d coins", q, d, n, p, m)
 }
 
 func main() {
+	cashier(1500)
 	cashier(1.17)
 }
--- reverse.go
+++ reverse.go
@@ -1,25 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func main() {
-	s := ""
-	fmt.Println("Please enter your text to revserse:")
-	fmt.Scanf("%s", &s)
-	fmt.Printf("entered : %s", s)
-	fmt.Println(reverse(s))
+func isEmpty(s string) bool {
+	return len(strings.TrimSpace(s)) == 0
 }
 
-func reverse(s string) string {
-	len := len(s)
+// How to take line from console : https://bit.ly/3iqK0nc, https://bit.ly/2DMnpm8
+func getLine(promptMessage string) (string, error) {
+	reader := bufio.NewReader(os.Stdin)
 
+	if !isEmpty(promptMessage) {
+		fmt.Print(promptMessage)
+	}
+
+	// input, er := reader.ReadString('\n')
+	// input2 := strings.TrimRight(input, "\n")
+
+	return reader.ReadString('\n')
+}
+
+func main() {
+	s, _ := getLine("Please enter your text to revserse:")
+	fmt.Print("entered : ")
+	fmt.Print(s)
+	var r1, r2 = reverse(s)
+	fmt.Printf("input: %s\n reverse1: %s \n reverse2: %s \n", s, r1, r2)
+}
+
+func reverse(s string) (string, string) {
+	len := len(s)
 	var r = make([]byte, len)
+	var r2 = make([]byte, len)
+	mid := int(len / 2)
 
 	for i := 0; i < len; i++ {
 		r[i] = s[len-1-i]
+		fmt.Printf("%c-%c\n", s[i], s[len-1-i])
 	}
 
-	reverse := string(r)
+	for i2 := 0; i2 < mid; i2++ {
+		lastIndex := len - 1 - i2
+		r2[i2] = s[lastIndex]
+		r2[lastIndex] = s[i2]
+	}
 
-	return reverse
+	r2[mid] = s[mid]
+
+	reverse := string(r)
+	reverse2 := string(r2)
+
+	return reverse, reverse2
 }
