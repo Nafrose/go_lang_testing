26d6e8f46c796578846f67d6b49eb7397da44e2b
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-13 18:12:13 +0600

    Improvements in the log and makefile

--- 06. cli-streamer/CLIStreamer.go
+++ 06. cli-streamer/CLIStreamer.go
@@ -17,7 +17,6 @@ func parseString(s string) string {
 }
 
 func outputFromCSVFile(s string) []string {
-
 	stringAfterSplit := strings.Split(s, ",")
 	title := stringAfterSplit[0]
 	msg1 := stringAfterSplit[1]
@@ -31,29 +30,26 @@ func outputFromCSVFile(s string) []string {
 }
 
 func streamingMultipleTimes(title, msg1, msg2, delay, iteration string) {
-
 	streamDelay, _ := strconv.Atoi(delay)
 	runTimes, _ := strconv.Atoi(iteration)
 	incident := 1
 
 	for i := 0; i < runTimes; i++ {
 		fmt.Printf("%s %v -> %s \n", title, incident, msg1)
-		incident++
-		time.Sleep(time.Duration(streamDelay) * time.Second)
 		fmt.Printf("%s %v -> %s \n", title, incident, msg2)
+		delayDurating := time.Duration(streamDelay) * time.Second
+		fmt.Printf("\n%s for %d second(s)...\n\n", "Waiting...", streamDelay)
+
 		incident++
+		time.Sleep(delayDurating)
 	}
 }
 
 func main() {
-
 	argumentsProvided := flag.String("args", "Title,Message 1,Message 2,Stream Delay,Run Times", "Write the argument")
 	flag.Parse()
-
 	argumentString := *argumentsProvided
-
 	argumentsStringAfterParsing := parseString(argumentString)
-
 	argumentsForStreaming := outputFromCSVFile(argumentsStringAfterParsing)
 
 	streamingMultipleTimes(
