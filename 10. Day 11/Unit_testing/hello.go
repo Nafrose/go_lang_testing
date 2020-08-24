be2897d1fa445f78a33e07fc5e5573f83b908e88
Nafrose <59255287+Nafrose@users.noreply.github.com>
2020-08-25 01:26:52 +0600

    Tasks of Day 11 (Partial)

---
+++ 10. Day 11/Unit_testing/hello.go
@@ -0,0 +1,12 @@
+package Unit_testing
+
+import "fmt"
+
+func hello(user string) string {
+	if len(user) == 0 {
+		return "Hello Dude!"
+	} else {
+		return fmt.Sprintf("Hello %v!", user)
+	}
+
+}
---
+++ 10. Day 11/composition-basic/ReadMe.md
@@ -0,0 +1,3 @@
+#### Use Basic Composition Task:
+
+Add properties (hasHair, legs, arms) and behaviours(walk, talk) to person using composition
