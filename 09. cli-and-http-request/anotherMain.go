1e43520ed0dde6bd88e655b8033e05f9a374fdc1
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-09-01 21:49:27 +0600

    Updated folder
    Updated folder

--- 09. cli-and-http-request/anotherMain.go
+++ 09. cli-and-http-request/anotherMain.go
@@ -1,14 +1,11 @@
 package main
 
-import (
-	_ "cli-and-http-request\clihelper\Structs"
-)
 
 func main() {
-	wC := WriterConfiguration{ShouldWriteToFile: true}
-	cliBindingProp := CliBinderProperties{CmdRunningInfo.IsAsync: true, Cmd : "cli-streamer.exe"}
-	// alex := PersonType{Id: 1, FirstName: "Alex", LastName: "Hales"}
-
-	cmd := exec.Command(cliBindingProp.Cmd, "", "")
-	RunAsync(cliBindingProp)
+	//wC := WriterConfiguration{ShouldWriteToFile: true}
+	//cliBindingProp := CliBinderProperties{CmdRunningInfo.IsAsync: true, Cmd : "cli-streamer.exe"}
+	//// alex := PersonType{Id: 1, FirstName: "Alex", LastName: "Hales"}
+	//
+	//cmd := exec.Command(cliBindingProp.Cmd, "", "")
+	//RunAsync(cliBindingProp)
 }
--- 09. cli-and-http-request/go.mod
+++ 09. cli-and-http-request/go.mod
@@ -1,3 +1,5 @@
 module github.com/nafrose/exploring/clirunner
 
 go 1.15
+
+require github.com/julienschmidt/httprouter v1.3.0
