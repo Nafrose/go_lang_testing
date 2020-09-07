632ccb7350a39f73ac143ed1e626d355265f5606
Nafrose <59255287+Nafrose@users.noreply.github.com>
2020-09-07 14:44:26 +0600

    updated import

--- 09. cli-and-http-request/commandlogger/default-loggers/DefaultOutputWriterImplementation.go
+++ 09. cli-and-http-request/commandlogger/default-loggers/DefaultOutputWriterImplementation.go
@@ -1,6 +1,11 @@
 package commandloggers
 
-import "log"
+import (
+	"log"
+
+	. "github.com/nafrose/exploring/clirunner/commandhelper/interfaces"
+	. "github.com/nafrose/exploring/clirunner/commandhelper/structs"
+)
 
 type DefaultOutputWriter struct{}
 
