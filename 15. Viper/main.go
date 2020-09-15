1c6fa5360117195611d684193f6682cd9b31fe93
Nafrose <59255287+Nafrose@users.noreply.github.com>
2020-09-15 11:42:19 +0600

    updated

--- 15. Viper/main.go
+++ 15. Viper/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigFile("./config/sysRequirement.json")
+	viper.SetConfigFile("./config/SysRequirement.json")
 
-	// Searches for config file in given paths and read it
+	// Searches for config file in given paths and reads it
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading config file, %s", err)
 	}
@@ -19,4 +20,39 @@ func main() {
 
 	fmt.Println(viper.GetFloat64("ram.minimumfreeSpace.size"))
 	fmt.Println(viper.GetFloat64("processor.maxUsages.percentage"))
+
+	// To write config to a predefined file (file existing already)
+	viper.SetConfigName("writeFile")
+	viper.SetConfigType("json")
+	viper.AddConfigPath("./config/")
+
+	// File path can also be set by
+	// viper.SetConfigFile("./config/writeFile.json")
+	viper.WriteConfig()
+
+	// alternatively can also write to given filepath. Creates the file if it doesn't exist
+	viper.SafeWriteConfigAs("./config/anotherWriteFile.json")
+
+	// Read values in struct
+	viper.SetConfigFile("./config/env.json")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	prod := viper.Sub("prod")
+
+	// Unmarshal into struct. Struct fields should match keys from config (case in-sensitive)
+	type config struct {
+		Host    string
+		Port    int
+		enabled bool
+	}
+
+	var C config
+
+	err := prod.Unmarshal(&C)
+	fmt.Println(err)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+	fmt.Println(C.Host)
 }
